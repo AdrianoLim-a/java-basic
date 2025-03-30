84ba008da0a456028bf31690c61bf0a1adbb1253
adriano lima <adriano.limaa@outlook.com>
2025-03-30 17:30:07 -0300

    Finaliza a aula de metodos

---
+++ sistema-smart-tv/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+
+    public static void main(String[] args) {
+        
+        SmartTv smartv = new SmartTv();
+        
+     
+        
+        smartv.diminuirVolume();
+        smartv.diminuirVolume();
+
+        System.out.println("TV Ligada?  " + smartv.ligada);
+
+        smartv.trocarCanal(29);
+        
+        System.out.println("Canal Atual? : " + smartv.canal);
+        
+        System.out.println("Volume Atual? : " + smartv.volume);
+
+        smartv.ligar();
+
+        System.out.println("TV Ligada?  " + smartv.ligada);
+
+        smartv.aumentarVolume();
+    }
+    
+}
--- tipos-variaveis/tipos-variaveis/src/OperadoresLogicos.java
+++ tipos-variaveis/tipos-variaveis/src/OperadoresLogicos.java
@@ -3,17 +3,12 @@ public class OperadoresLogicos {
         
         boolean condicao1 = true;
         boolean condicao2 = false;
-
-
         if(condicao1 && (7 > 4)){
             System.out.println("as duas condições são verdadeiras na primeira");
         }
-
         if(condicao1 || condicao2){
             System.out.println("as duas condições são verdadeiras na segunda");
         
-
-
     }
 }
     
