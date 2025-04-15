b58beb0bc3d2d99f87e5072700361f986e01e8d2
adriano lima <adriano.limaa@outlook.com>
2025-04-14 21:31:04 -0300

    Finaliza modulo de estrutura condicional

--- controle-fluxo/controle-fluxo/src/App.java
+++ controle-fluxo/controle-fluxo/src/App.java
@@ -1,7 +1,5 @@
-package edu.adriano.primeirasemana;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas Vindas a todos");
-
+        System.out.println("Hello, World!");
     }
 }
