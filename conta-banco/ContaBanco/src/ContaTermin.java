b187497f5f8a493594e39695bae93e88e1fb4bb9
adriano lima <adriano.limaa@outlook.com>
2025-04-20 13:54:20 -0300

    Finaliza desafio terminal

---
+++ conta-banco/ContaBanco/src/ContaTermin.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ContaTermin {
+    public static void main(String[] args) {
+        
+        int agencia = 1021;
+        String conta ="067-8";
+        String nomeCliente = "Adriano Lima";
+        double saldo = 237.48;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Por favor, digite o número da Agência");
+ 
+            int agenciaVerificador = scanner.nextInt();
+            
+            System.out.println("O numero da sua Conta");
+ 
+            String contaVerificador = scanner.next();
+            
+            if(agenciaVerificador == agencia  && (contaVerificador.equals(conta))){
+                System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco");
+                System.out.println("A sua Agência e Conta são: " + agencia + " / " + conta);
+                System.out.println("O saldo disponivel e atualizado é de R$ " + saldo);
+            }else{
+                System.out.println("Conta inexistente, tente novamente");
+            }
+        }
+        
+    }
+}
